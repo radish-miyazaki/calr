f16c8992e3f728a5c77cc589d8b8edc23b8cb25a
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-03-08 10:17:10 +0900

    fix shell script & re-generate output files

--- mk-outs.sh
+++ mk-outs.sh
@@ -3,7 +3,7 @@
 OUTDIR="tests/expected"
 [[ ! -d "$OUTDIR" ]] && mkdir -p "$OUTDIR"
 
-cal 2024 > $OUTDIR/2024.txt
-cal 2 2024 > $OUTDIR/2-2024.txt
-cal 4 2024 > $OUTDIR/4-2024.txt
-cal 5 2024 > $OUTDIR/5-2024.txt
+cal 2023 > $OUTDIR/2023.txt
+cal 2 2020 > $OUTDIR/2-2020.txt
+cal 4 2023 > $OUTDIR/4-2023.txt
+cal 5 2023 > $OUTDIR/5-2023.txt
